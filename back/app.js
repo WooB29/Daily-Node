ae0a261d1700cf6fba91c430a6f1f316d96c37eb
WooB29 <tmxk0127@naver.com>
2024-03-02 04:31:54 +0900

    five

--- back/app.js
+++ back/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+//const cors = require('cors');
+//const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { Pool } = require('pg');
+const pgdb = new Pool({
+    user: 'postgres',
+    password: '1234',
+    database: 'daily',
+    host: 'postgres',
+});
+
+pgdb.on('connect', (client) => {
+    client
+    .query("CREATE TABLE IF NOT EXISTS todoList (id SERIAL PRIMARY KEY, text VARCHAR(255), done BOOLEAN)")
+    .catch(err => console.log(err));
+});
+
 /*
 mongoose.connect('mongodb://localhost:27017/daily', {useNewUrlParser: true}, (err) => {
     if(err){
@@ -13,31 +30,51 @@ mongoose.connect('mongodb://localhost:27017/daily', {useNewUrlParser: true}, (er
     }
 });
 */
-app.use(cors());
+//app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// let id = 2;
+// const todoList = [{
+//     id: 1,
+//     text: '할일 1',
+//     done: false,
+// }];
 
-let id = 2;
-const todoList = [{
-    id: 1,
-    text: '할일 1',
-    done: false,
-}];
-
-app.get('/todo', (req, res) => {
-    res.json(todoList);
+app.get('/todo', async (req, res) => {
+    try {
+        const todoList = await pgdb.query('SELECT * FROM todoList');
+        res.json(todoList.rows);
+    } 
+    catch (error) {
+        console.error('Error fetching todo list:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    //res.json(todoList);
 });
 
-app.post('/todo', (req, res) => {
-    const { text, done } = req.body;
-    console.log('req.body : ', req.body);
-    todoList.push({
-        id: ++id,
-        text,
-        done,
-    });
-    return res.send('Success!!');
+app.post('/todo', async (req, res) => {
+    try {
+        const { text, done } = req.body;
+        const query = {
+            text: 'INSERT INTO todoList (text, done) VALUES($1, $2) RETURNING *',
+            values: [text, done]
+        };
+        await pgdb.query(query);
+        
+        const resp = await pgdb.query('SELECT * FROM todoList');
+        res.json(resp.rows);
+    } 
+    catch (error) {
+        console.error('Error inserting todo:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    // const { text, done } = req.body;
+    // console.log('req.body : ', req.body);
+    // todoList.push({
+    //     id: ++id,
+    //     text,
+    //     done,
+    // });
+    // return res.send('Success!!');
 });
 
 app.listen((port), () => {
